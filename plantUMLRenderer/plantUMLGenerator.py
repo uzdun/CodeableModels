ef295b68f24ee3dd4beb63f05cceb7381fd9013c
Uwe Zdun <uwe.zdun@univie.ac.at>
2019-08-21 12:06:53 +0200

    plantUMLGenerator configuration options: setters

--- plantUMLRenderer/plantUMLGenerator.py
+++ plantUMLRenderer/plantUMLGenerator.py
@@ -5,25 +5,23 @@ import shutil
 
 class PlantUMLGenerator(object):
     def __init__(self, deleteGenDir = True):
-        self.directory = "../_generated"
-        self.plantUmlJarPath = "../../libs/plantuml.jar"
+        self._directory = "../_generated"
+        self._plantUmlJarPath = "../../libs/plantuml.jar"
         if deleteGenDir:
             if os.path.exists(self.directory):
                 shutil.rmtree(self.directory)
             os.makedirs(self.directory)
-        self.classModelRenderer = ClassModelRenderer(plantUmlJarPath = self.plantUmlJarPath, directory = self.directory)
-        self.objectModelRenderer = ObjectModelRenderer(plantUmlJarPath = self.plantUmlJarPath, directory = self.directory)
+        self.classModelRenderer = ClassModelRenderer(plantUmlJarPath = self._plantUmlJarPath, directory = self._directory)
+        self.objectModelRenderer = ObjectModelRenderer(plantUmlJarPath = self._plantUmlJarPath, directory = self._directory)
 
     def getFileName(self, elementName):
         name = elementName.replace(' ', '_')
         return name
 
     def generateClassModel(self, bundle, **kwargs): 
-        self.classModelRenderer.directory = self.directory
         self.classModelRenderer.renderClassModelToFile(self.getFileName(bundle.name), bundle.elements, **kwargs)
 
-    def generateObjectModel(self, bundle, **kwargs):
-        self.objectModelRenderer.directory = self.directory
+    def generateObjectModel(self, bundle, **kwargs): 
         self.objectModelRenderer.renderObjectModelToFile(self.getFileName(bundle.name), bundle.elements, **kwargs)
 
     def generateClassModels(self, dirName, viewList):
@@ -40,3 +38,20 @@ class PlantUMLGenerator(object):
             self.generateObjectModel(bundle, **kwargs)
         self.directory = mainDir
 
+    @property
+    def plantUmlJarPath(self):
+        return self._plantUmlJarPath
+    @plantUmlJarPath.setter
+    def plantUmlJarPath(self, plantUmlJarPath):
+        self._plantUmlJarPath = plantUmlJarPath
+        self.classModelRenderer.plantUmlJarPath = plantUmlJarPath
+        self.objectModelRenderer.plantUmlJarPath = plantUmlJarPath
+
+    @property
+    def directory(self):
+        return self._directory
+    @directory.setter
+    def directory(self, directory):
+        self._directory = directory
+        self.classModelRenderer.directory = directory
+        self.objectModelRenderer.directory = directory
