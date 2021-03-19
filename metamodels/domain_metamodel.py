03665cd76a5459108b597f334e322590f2d3b691
uwe <uwe.zdun@univie.ac.at>
2021-03-19 10:29:59 +0100

    some corrections and extensions in meta-models

--- metamodels/domain_metamodel.py
+++ metamodels/domain_metamodel.py
@@ -10,8 +10,8 @@ from codeable_models import CMetaclass, CBundle
 
 domain_metaclass = CMetaclass("Domain Class")
 domain_metaclass_group = CMetaclass("Domain Class Group", superclasses=domain_metaclass)
-and_combined_group = CMetaclass("And-Combined Group", superclasses=domain_metaclass_group)
-or_combined_group = CMetaclass("Or-Combined Group", superclasses=domain_metaclass_group)
+and_combined_group = CMetaclass("And-Combined Domain Class Group", superclasses=domain_metaclass_group)
+or_combined_group = CMetaclass("Or-Combined Domain Class Group", superclasses=domain_metaclass_group)
 domain_metaclass_group.association(domain_metaclass, "[collection] * <>- [class] *")
 
 domain_metamodel = CBundle("Domain Meta Model", elements=domain_metaclass.get_connected_elements())
